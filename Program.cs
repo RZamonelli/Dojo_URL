50224e5f8b410b3abaf2be62c014bf74c7360774
Renan Zamonelli <47351828+RZamonelli@users.noreply.github.com>
2020-06-14 12:22:29 -0300

    Final

--- Program.cs
+++ Program.cs
@@ -1,36 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace URL
 {
+    class ProcessURL
+    {
+        private static ProcessURL _instance;
+
+        static ProcessURL()
+        {
+            _instance = new ProcessURL();
+        }
+
+        public static ProcessURL Instance()
+        {
+            return _instance;
+        }
+
+        public void AnalyzeURL(string url)
+        {
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+           int counter = 0;
+           string line;
+
+           using (StreamReader sr = ReadListUrl())
+           {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    try
+                    {
+                        string baseUrl = new UriBuilder(line).ToString();
+                        ProcessURL.Instance().AnalyzeURL(baseUrl);                        
+                    }
+                    catch (UriFormatException e)
+                    {
+                        Console.WriteLine(e.Message + " URL: " + line);
+                    }                 
+                 
+                    counter++;
+               }
+            }
+
+           Console.ReadKey();
+        }
+
+        private static StreamReader ReadListUrl()
+        {
             try
             {
-                int counter = 0;
-                string line;
-
-                using (StreamReader sr = new StreamReader(@"C:\Users\Renan\Documents\GitHub\Dojo_URL\Util\url.txt"))//change this path
-                {
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        string decodedUrl = Uri.UnescapeDataString(line);
-                        UriBuilder baseUri = new UriBuilder(decodedUrl);
-                        Console.WriteLine(baseUri);
-                        counter++;
-                    }
-                }
+              return new StreamReader(@"C:\Users\Renan\Documents\GitHub\Dojo_URL\Util\url.txt");//change this path              
             }
             catch (IOException e)
             {
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
+                return null;
             }
-
-            Console.ReadKey();
         }
-
     }
 }
